4c2bc9bfdb8fbbdc27ab2b42d0b82c7fe7fa1300
Scott Sawchuk <scott.sawchuk@alleninstitute.org>
2025-08-22 16:45:31 -0700

    summary figure and analysis build

--- dev/output_analysis.R
+++ dev/output_analysis.R
@@ -49,19 +49,87 @@ MDanalysis = MD[MD$cell_name %in% for_analysis,]
 my_list = jsonlite::fromJSON(cell_json[1])
 my_list = jsonlite::fromJSON(cell_json[3])
 plot(x = my_list$spike_puff0$spike_times, y = my_list$spike_puff0$Instantaneous_frequency)
+i = cells[3]
 
-
-i = cells[1]
 
 for(i in cells){
 
   iMD = MD[grep(i, MD$cell_name),]
   srt = loadHCT(i)
-
-  srt$f$nwb
-
+  iMerged = merged_metadata[grep(i, merged_metadata$cell_name),]
   jfile = cell_json[grep(i, cell_json)]
   jlist = jsonlite::fromJSON(jfile)
+
+  if("baseline_spike_puff" %in% iMerged$stimulus_description){
+
+    # Baseline Sweep
+    n = iMerged[which(iMerged$stimulus_description == "baseline_spike_puff"), "wrapped_sweeps"]
+    n = as.numeric(gsub("\\[|\\]", "", n))
+    tst = extractHCT(srt$files$nwb, sweeps = n+1, slim = TRUE)
+    #plot(tst, type = "l")
+    fp = nphys::APanalysis(tst, rate = srt$exp$smry$sampling_rate[n+1])
+    fp = as.data.frame(fp)
+
+    # Bin averages of baseline sweep
+    out1 = fp %>% mutate(bin = cut(epoch_t, breaks = seq(0, max(epoch_t), by = bin_width), right = FALSE)) %>%
+      group_by(bin) %>%
+      summarise(count = n(),
+                rate = count / bin_width)
+    out1 = out1[-nrow(out1),]
+
+    # Stimulus sweep
+    n2 = iMerged[which(iMerged$stimulus_description == "spike_puff"), "wrapped_sweeps"]
+    n2 = as.numeric(gsub("\\[|\\]", "", n2))
+    tst2 = extractHCT(srt$files$nwb, sweeps = n2+1, slim = TRUE)
+    #plot(tst2, type = "l")
+
+    fp2 = nphys::APanalysis(tst2, rate = srt$exp$smry$sampling_rate[n2+1])
+    fp2 = as.data.frame(fp2)
+    bins2 <- cut(fp2$epoch_t, breaks = breaks, right = FALSE)
+
+    table(bins2)
+
+    out2 = fp2 %>% mutate(bin = cut(epoch_t, breaks = seq(0, 55, by = bin_width), right = FALSE)) %>%
+      group_by(bin) %>%
+      summarise(count = n(),
+                rate = count / bin_width)
+    out2 = out2[-nrow(out2),]
+
+
+    pCh = as.numeric(out1[nrow(out1), "rate"])
+    df = rbind(out1[6:11,], out2)
+    df$pro = c(rep("baseline", 7), rep("stimulus", 10))
+
+    df$percent_change = df$rate/pCh*100
+
+    df$bin = NULL
+    df$count = NULL
+
+    df = as.data.frame(df)
+    df$time = seq(-30,50, by = 5)
+
+    ggplot(df, aes(x = time, y = percent_change, colour = pro)) +
+      geom_point() +
+      ylim(0, max(df$percent_change)) +
+      theme_minimal()
+
+    #plotVoid(tst)
+    #plotVoid(tst2)
+
+    comparison <- data.frame(
+      bin = 1:n_bins,
+      stim_rate = stim_rate,
+      baseline_rate = rep(baseline_rate, n_bins)
+    )
+  }
+
+  df = data.frame(time = fp$epoch_t)
+  bin_width = 5
+
+  df %>% mutate(bin = cut(time, breaks = seq(0, max(time)+bin_width, by = bin_width), right = FALSE)) %>%
+    group_by(bin) %>%
+    summarise(count = n(),
+              rate = count / bin_width)
 
   smry = srt$exp$smry
   smry[jlist$spike_puff0$sweeps_analyzed+1,]
@@ -78,14 +146,5 @@ for(i in cells){
   plot(x = jlist$spike_puff0$spike_times, y = jlist$spike_puff0$Instantaneous_frequency)
 
 }
-
-msry = projHCT::sheets$dfs$massSmry
-
-unique(msry$stimulus_description)
-
-puffPros = msmry[grep("50spuff", msmry$stimulus_description),]
-
-length(unique(puffPros$cell_name))
-
 
 
